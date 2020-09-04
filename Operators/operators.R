d30e0b8778000a67f8eff039abe64d8a01bf2853
aravindhakshan0880 <aravind.000.as@gmail.com>
2020-09-04 21:14:07 +0530

    Operators in R

---
+++ Operators/operators.R
@@ -0,0 +1,26 @@
+#Arthematic Operators (same as any language)
+x = 5 #ASSIGN
+y = 2
+x + y #add
+x - y #Sub
+x %% y #remainder
+x / y #divide
+x %/% y # quoitent
+x ^ y #exponent
+
+#Relational Operators (same as any language)
+x < y
+x > y
+x <= y
+x >= y
+x == y
+x != y
+
+#Logical Operators
+f = 14
+
+f > 5 & f <22                  # &,|, !,
+f == 9 | f < 0
+
+is.numeric(f)
+!is.numeric(5)
---
+++ Operators/operators_2.R
@@ -0,0 +1,3 @@
+!c(TRUE,FALSE,FALSE)
+!c(TRUE,FALSE,FALSE) & c(FALSE,FALSE,FALSE)
+#Vectors using relational Operators
