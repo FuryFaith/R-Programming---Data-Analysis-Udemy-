32c6affb120d2da13832f96bcdda258044559c61
aravindhakshan0880 <aravind.000.as@gmail.com>
2020-09-20 13:58:45 +0530

    Descrpitive Statements

---
+++ Descriptive Statistics in R/Descriptive.R
@@ -0,0 +1,20 @@
+#Mean: mean()
+#Standard deviation : sd()
+#Variance: var()
+#minimum: min()
+#maximum: maximum()
+#Median: median()
+#Range of values from min to max : range()
+#Sample quantiles: quantile()
+#Generic function : summary()
+#Row and Column sum: rowSums() and colSums()
+#Row and Column means: rowMeans() and colMeans()
+
+#Compute the mean value
+mean(mtcars$mpg)
+
+#Compute the median value
+median(mtcars$mpg)
+
+#Compute the variance value
+var(mtcars$mpg)
