240a69254a4b42711f2d69c0b27d308d9606403b
aravindhakshan0880 <aravind.000.as@gmail.com>
2020-09-23 14:32:48 +0530

    Descriptive Statistics

--- Descriptive Statistics in R/Descriptive.R
+++ Descriptive Statistics in R/Descriptive.R
@@ -18,3 +18,38 @@ median(mtcars$mpg)
 
 #Compute the variance value
 var(mtcars$mpg)
+
+#Computes the minimum value
+min(mtcars$mpg)
+
+#Compute the standarddeviation
+sd(mtcars$mpg)
+
+#Compute the maximum value
+max(mtcars$mpg)
+
+#Compute range
+range(mtcars$mpg)
+
+#Generic Function
+summary(mtcars$mpg)
+
+#interquantile range
+
+quantile(mtcars$mpg, probs = seq(0, 1, 0.25))
+
+#Column and Row sum, Column and Row means
+df = data.frame(a=c(1,2,3,4,5,6,7), b=c(7,6,5,4,3,2,4))
+
+#Colum Sum
+colSums(df)
+
+#Row Sum
+rowSums(df)
+
+#Column Mean
+colMeans(df)
+
+#Row Mean
+
+rowMeans(df)
